1e82dee3f733f3dfbe4f3f9f5a940ea22bf18d1c
malte <malte.budig@gmx.net>
2024-03-29 16:45:26 +0100

    switchted to autostart

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
 
-apt-get install streamlink
-apt-get install vlc
+sudo apt-get install streamlink
+sudo apt-get install vlc
 
-mkdir /usr/local/livestream-viewer
-cd /usr/local/livestream-viewer
-wget -O /usr/local/livestream-viewer/start-livestream-viewer.sh www.google.de
+
+url=$HOME/livestream-viewer 
+mkdir $url
+cd $url
+wget -O $url/start-livestream-viewer.sh https://raw.githubusercontent.com/Doderich/livestream-viewer/main/start-livestream-viewer.sh
+wget -O $url/auto-start-entry.txt https://raw.githubusercontent.com/Doderich/livestream-viewer/main/auto-start-entry.txt
 
 echo "Please enter the livestream url"
-read livestream_url
+#read livestream_url
+livestream_url="https://www.youtube.com/UCA_oHukmsUBrp4GMoxG04Nw/live"
+#livestream=$livestream_url
+cat start-livestream-viewer.sh | sed -e "s/livestream_url/$livestream_url/g" > lol.txt
 
-livestream=$livestream_url
-echo ./start-livestream-viewer.sh | start- sed -i -e 's/$livestream_url/$livestream/g' | echo
-
-/bin/bash /usr/local/livestream-viewer/start-livestream-viewer.sh
+mkdir $HOME/.config/autostart
+touch $HOME/.config/autostart/livestream-viewer.desktop
+cat auto-start-entry.txt > $HOME/.config/autostart/livestream-viewer.desktop
+#/bin/bash /usr/local/livestream-viewer/start-livestream-viewer.sh
+cat $HOME/.config/autostart/livestream-viewer.desktop
 
 echo 
 #[Desktop Entry]
