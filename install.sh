9ab753700121a4dcef4643fd3a8c3ec54cd2287f
malte <malte.budig@gmx.net>
2024-03-29 18:21:10 +0100

    added home sting

--- install.sh
+++ install.sh
@@ -7,20 +7,22 @@ sudo apt-get install vlc
 url=$HOME/livestream-viewer 
 mkdir $url
 cd $url
+
 wget -O $url/start-livestream-viewer.sh https://raw.githubusercontent.com/Doderich/livestream-viewer/main/start-livestream-viewer.sh
 wget -O $url/auto-start-entry.txt https://raw.githubusercontent.com/Doderich/livestream-viewer/main/auto-start-entry.txt
 
 echo "Please enter the livestream url"
-#read livestream_url
+read livestream_url
+
 livestream_url="https://www.youtube.com/UCA_oHukmsUBrp4GMoxG04Nw/live"
-#livestream=$livestream_url
-cat start-livestream-viewer.sh | sed -e "s/livestream_url/$livestream_url/g" > lol.txt
 
 mkdir $HOME/.config/autostart
 touch $HOME/.config/autostart/livestream-viewer.desktop
-cat auto-start-entry.txt > $HOME/.config/autostart/livestream-viewer.desktop
+home_string="$HOME"
+sed -i "s/HOME_STRING/$home_string/g" $url/auto-start-entry.txt
+cat $url/auto-start-entry.txt > $HOME/.config/autostart/livestream-viewer.desktop
 #/bin/bash /usr/local/livestream-viewer/start-livestream-viewer.sh
-cat $HOME/.config/autostart/livestream-viewer.desktop
+chmod +x $HOME/.config/autostart/livestream-viewer.desktop
 
 echo 
 #[Desktop Entry]
--- start-livestream-viewer.sh
+++ start-livestream-viewer.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-livestream_url="https://www.youtube.com/watch?v=dx-IEo_iCZA"
+livestream_url=https://www.youtube.com/channel/UCA_oHukmsUBrp4GMoxG04Nw/live
+
+while getopts url flag
+do
+    case "${flag}" in
+        u) livestream_url=${OPTARG};;
+    esac
+done
+
 retry_time_s=60
 while :; do streamlink $livestream_url best -p vlc -a "-f" --retry-streams $retry_time_s; sleep $retry_time_s; done
-#echo $date >> /home/parus/projects/echo-log.txt
